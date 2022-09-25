7a3d136017a26c576b98d998c1c96ecf5c7698e3
Russ Henson <henson.russ10@gmail.com>
2022-09-25 11:48:47 +0800

    feat: view specific show

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/index";
+import { Home, Show } from "./pages/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/show/:id" element={<Show />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,30 @@
 import { BoxContainer, Search, ShowImage } from "../components/index";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, CircularProgress } from "@mui/material";
 import { useState } from "react";
 
 export const Home = () => {
   const [searchValue, setSearchValue] = useState("");
   const [resultData, setResultData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = () => {
+    setIsLoading(true);
     fetch(`https://api.tvmaze.com/search/shows?q=${searchValue}`, {
       method: "GET",
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         const filtered = data.filter((data) => data.show.image !== null);
         setResultData(
           filtered.map((item) => ({
+            id: item.show.externals.imdb,
             name: item.show.name,
             image: item.show.image.medium,
           }))
         );
         setSearchValue("");
+        setIsLoading(false);
       })
       .catch((err) => {
         console.error(err);
@@ -36,7 +41,7 @@ export const Home = () => {
         mx: "auto",
       }}
     >
-      <Grid item sx={{ mt: 3, width: "100%" }} >
+      <Grid item sx={{ mt: 3, width: "100%" }}>
         <BoxContainer>
           <Search
             searchValue={searchValue}
@@ -50,12 +55,16 @@ export const Home = () => {
           />
         </BoxContainer>
       </Grid>
-      <Grid item sx={{width: "100%"}}>
+      <Grid item sx={{ width: "100%" }}>
         <BoxContainer>
-          {resultData.length !== 0 ? (
+          {isLoading ? (
+            <CircularProgress sx={{ py: 3 }}/>
+          ) : resultData.length !== 0 ? (
             <ShowImage data={resultData} />
           ) : (
-            <Typography color="#FFF" sx={{py: 3}}>No Result</Typography>
+            <Typography color="#FFF" sx={{ py: 3 }}>
+              No Match Found. Search a keyword or title.
+            </Typography>
           )}
         </BoxContainer>
       </Grid>
